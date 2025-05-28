064f831ec7f23ac409fad0bcf20248567846a79d
Rimjhimrani <144531476+Rimjhimrani@users.noreply.github.com>
2025-05-28 09:33:14 +0530

    Update InstorLabel.py

--- InstorLabel.py
+++ InstorLabel.py
@@ -329,9 +329,10 @@ def main():
     )
     
     st.title("🏷️ INSTOR LABEL GENERATOR")
+
     # Add the developer credit in smaller italic text on the left
     st.markdown(
-        "<p style='font-size:14px; font-style:italic; margin-top:-10px;'>"
+        "<p style='font-size:14px; font-style:italic; margin-top:-10px; text-align:left;'>"
         "designed and developed by Agilomatrix</p>",
         unsafe_allow_html=True
     )
